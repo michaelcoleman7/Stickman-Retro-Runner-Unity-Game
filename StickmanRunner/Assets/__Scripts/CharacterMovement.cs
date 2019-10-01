03a2ef1b76c40a88eadd31cdb6ff60f4f36a32fe
Michael Coleman <michaelcoleman135@gmail.com>
2019-10-01 19:25:31 +0100

    Chnaged Character movement to constant

--- StickmanRunner/Assets/__Scripts/CharacterMovement.cs
+++ StickmanRunner/Assets/__Scripts/CharacterMovement.cs
@@ -60,16 +60,6 @@ public class CharacterMovement : MonoBehaviour
 
     private void Move()
     {
-        // find the change signal from the keyboard/input device
-        // create a value for the change
-        // Time.deltaTime - frame rate independent - same experience
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxis("Horizontal") * Time.deltaTime;
-
-        // add the change to the current position
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
-
-        transform.position = new Vector2(newXPos, newYPos);
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
     }
 }
