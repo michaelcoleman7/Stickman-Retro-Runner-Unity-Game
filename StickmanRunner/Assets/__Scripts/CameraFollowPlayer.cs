06f1053c95a975a41907e13b47a9caefbc32c77c
Michael Coleman <michaelcoleman135@gmail.com>
2019-10-01 17:44:13 +0100

    Added scripts which failed to upload

---
+++ StickmanRunner/Assets/__Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float cameraDistance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //change the camera size to allow fitting of the screen
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Move the camera with the players current coordinates
+        transform.position = new Vector3(player.position.x + 20, player.position.y, transform.position.z);
+    }
+}
---
+++ StickmanRunner/Assets/__Scripts/CharacterMovement.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // == public fields ==
+    // == private fields ==
+    [SerializeField]
+    private float moveSpeed = 15f;
+    public float jumpForce;
+    private Rigidbody2D rb;
+
+    private bool onGround;
+    public Transform PlatformCheck;
+    public float checkRadius;
+    public LayerMask platform;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // == private methods ==
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump when space pressed or mouse clicked user should jump
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown((0)) && onGround)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        //Can add double jump code - need to discuss with client
+        /*//Jump when space pressed and user still has a double jump left
+        if (Input.GetKeyDown(KeyCode.Space) && doubleJump > 0) 
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            doubleJump--;
+        }
+        //Jump when space pressed and user but has no double jump left
+        else if (Input.GetKeyDown(KeyCode.Space) && doubleJump == 0 && onGround)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }*/
+
+    }
+
+    void FixedUpdate()
+    {
+        // Have detection for when the user is on the ground using a game object placed at players feet.
+        onGround = Physics2D.OverlapCircle(PlatformCheck.position, checkRadius, platform);
+
+        //Call player movement function
+        Move();
+    }
+
+
+    private void Move()
+    {
+        // find the change signal from the keyboard/input device
+        // create a value for the change
+        // Time.deltaTime - frame rate independent - same experience
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        // add the change to the current position
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
