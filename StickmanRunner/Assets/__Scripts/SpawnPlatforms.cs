a62b0ff59bd6059ad1c0db617ab0a0107557c204
Michael Coleman <michaelcoleman135@gmail.com>
2019-10-04 15:39:21 +0100

    Added random distances between platform spawns

--- StickmanRunner/Assets/__Scripts/SpawnPlatforms.cs
+++ StickmanRunner/Assets/__Scripts/SpawnPlatforms.cs
@@ -6,7 +6,9 @@ public class SpawnPlatforms : MonoBehaviour
 {
     public GameObject platform;
     public Transform spawnPoint;
-    public float distance;
+    private float distance;
+    public float minDistance;
+    public float maxDistance;
     private float platformWidth;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,9 @@ public class SpawnPlatforms : MonoBehaviour
     {
         if (transform.position.x < spawnPoint.position.x)
         {
+            //set distance to random number between two values given
+            distance = Random.Range(minDistance, maxDistance);
+
             //Change the spawnpoint of the platforms equal to the distance set between them + the width of the platforms,
             //so that they spawn off screen for the user's experience.
             transform.position = new Vector3(transform.position.x + platformWidth + distance, transform.position.y, 0);
