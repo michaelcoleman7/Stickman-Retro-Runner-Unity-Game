bc813704f6e2b18035e8a076d81363146861b262
Michael Coleman <michaelcoleman135@gmail.com>
2019-10-08 19:18:45 +0100

    Added Traps to the game

--- StickmanRunner/Assets/__Scripts/SpawnPlatforms.cs
+++ StickmanRunner/Assets/__Scripts/SpawnPlatforms.cs
@@ -16,6 +16,10 @@ public class SpawnPlatforms : MonoBehaviour
     public Transform maxSpawnHeight;
     private float heightDifference;
     public float maxHeightDifference;
+
+    public GameObject trapPlatform1;
+    public GameObject trapPlatform2;
+    private float trapRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,7 +52,32 @@ public class SpawnPlatforms : MonoBehaviour
             transform.position = new Vector3(transform.position.x + platformWidth + distance, heightDifference, 0);
 
             //spawn the platforms in, at the new spawnpoint
+            //Instantiate(trap, transform.position, transform.rotation);
             Instantiate(platform, transform.position, transform.rotation);
+
+            //set trapRange to a random value between 1 and 10
+            trapRange = Random.Range(0, 10);
+
+            if (trapRange > 5)
+            {
+                //set random value to boolean variable - https://gamedev.stackexchange.com/questions/110332/is-there-a-random-command-for-boolean-variables-in-unity-c
+                bool leftTrap = (Random.value > 0.5f);
+                if (leftTrap)
+                {
+                    //spawn in trap platform 1
+                    Instantiate(trapPlatform1, transform.position, transform.rotation);
+                }
+                else
+                {
+                    //spawn in trap platform 2
+                    Instantiate(trapPlatform2, transform.position, transform.rotation);
+                }
+            }
+            else 
+            {
+                ////spawn in regular platform
+                Instantiate(platform, transform.position, transform.rotation);
+            }
         }
     }
 }
