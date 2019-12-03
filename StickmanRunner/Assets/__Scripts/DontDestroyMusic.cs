5a421aff11056a01355f465e8e43fd6ff1063975
Michael Coleman <michaelcoleman135@gmail.com>
2019-12-03 16:23:51 +0000

    Cleaned and commented MusicController

--- StickmanRunner/Assets/__Scripts/DontDestroyMusic.cs
+++ StickmanRunner/Assets/__Scripts/DontDestroyMusic.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DontDestroyMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Method to ensure object is not destroyed when level change occurs
     void Awake()
     {
+        //search for all objects with Music tag and assign to object array
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
         if (objs.Length > 1)
         {
+            //Destroy object
             Destroy(this.gameObject);
         }
-
+        // adapted from https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html
         DontDestroyOnLoad(this.gameObject);
     }
 }
